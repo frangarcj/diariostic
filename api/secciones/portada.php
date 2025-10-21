d6a578f726c3cda19e12132b9a67da354d6259ad
Francisco José García García <frangarcj@gmail.com>
2025-10-21 17:00:14 +0200

    Update portada.php author and summary

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -5,7 +5,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 
 $portada = [
 "titulo" => "Portada",
-"autor" => "mtorres",
-"resumen" => "El resumen maravilloso",
+"autor" => "paco.garcia",
+"resumen" => "El resumen no es tan maravilloso",
 ];
 ?>
